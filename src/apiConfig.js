a5a49c8ab2acf4d2074997072dcc99f452591dd0
Auriga08 <152512108+Auriga08@users.noreply.github.com>
2025-09-24 18:07:19 +0700

    Update apiConfig.js

--- src/apiConfig.js
+++ src/apiConfig.js
@@ -2,7 +2,7 @@
 import axios from 'axios';
 
 const apiClient = axios.create({
-  baseURL: 'https://rri-backend-xxxx.vercel.app/api' // Ganti dengan URL Vercel Anda
+  baseURL: 'https://rri-backend.vercel.app/' // Ganti dengan URL Vercel Anda
 });
 
 export default apiClient;
